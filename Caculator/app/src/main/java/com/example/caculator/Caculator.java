0d526cf562a1c520053763844b545131e6dfbb69
wjywjywjywjy <1150445570@qq.com>
2019-09-08 19:13:34 +0800

    reuse

---
+++ Caculator/app/src/main/java/com/example/caculator/Caculator.java
@@ -0,0 +1,14 @@
+package com.example.caculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Caculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+    }
+}
